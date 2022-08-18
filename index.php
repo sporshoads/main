5ed8546d4bce65a5ba0e88d9b889b95aa6d8dda8
sporshoads <111539712+sporshoads@users.noreply.github.com>
2022-08-19 03:56:04 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+$url = "#";
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Watch my vid...</title>
+  </head>
+  <body>
+    <div class="container">
+      <h4 class="text-center" >Hi I am Asha, Watch my full video</h4>
+    </div>
+     
+     
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-lg-6">
+          <div class="card">
+            <a href="<?php echo $url; ?>">
+              <img
+              src="img/1.gif"
+              class="card-img-top"
+              alt="Waterfall"
+            />
+            </a> <br/>
+
+            <a href="<?php echo $url; ?>">
+              <img
+              src="img/b1.png"
+              class="card-img-top"
+              alt="Waterfall"
+            />
+            </a> <br/>
+
+            <a href="<?php echo $url; ?>">
+              <img
+              src="img/b2.jpg"
+              class="card-img-top"
+              alt="Waterfall"
+            />
+            </a>
+            
+          </div>
+        </div>
+        
+        <div class="col-lg-6">
+          <div class="card">
+            <a href="<?php echo $url; ?>">
+              <img
+              src="img/2.gif"
+              class="card-img-top"
+              alt="Waterfall"
+            />
+            </a> <br/>
+
+            <a href="<?php echo $url; ?>">
+              <img
+              src="img/b1.png"
+              class="card-img-top"
+              alt="Waterfall"
+            />
+            </a> <br/>
+
+            <a href="<?php echo $url; ?>">
+              <img
+              src="img/b2.jpg"
+              class="card-img-top"
+              alt="Waterfall"
+            />
+            </a>
+            
+          </div>
+        </div>
+
+      </div>
+    </div>
+    
+    <br/> <br/> <br/>
+
+    
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
